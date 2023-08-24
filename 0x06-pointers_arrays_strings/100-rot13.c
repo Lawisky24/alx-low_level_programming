12a2e5416b01a39d5bb3300a1b00ceca19cff590
Lawisky24 <onyelawnwa@gmai.com>
2023-08-24 13:38:23 -0700

    rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,11 +1,29 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * rot13 - function that encodes a string using rot13
- * @str; function parameter
- * Return: 
+ * @s: function parameter
+ * Return: *s
  */
 
-char *rot13(char *str)
+char *rot13(char *s)
 {
+	int i;
+	int j;
+	char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == data1[j])
+			{
+				s[i] = datarot[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
