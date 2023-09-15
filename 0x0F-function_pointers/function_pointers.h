b429236f8bd7ac7753dd029d99503e839d988e18
Lawisky24 <onyelawnwa@gmai.com>
2023-09-15 08:28:54 -0700

    A goal

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -2,9 +2,19 @@
 #define MAIN_H
 #include <stdio.h>
 #include "function_pointers.h"
+#include "3-calc.h"
+#include <stdlib.h>
 
+int (*f)(int a, int b);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
+int _putchar(char c);
 
 #endif
