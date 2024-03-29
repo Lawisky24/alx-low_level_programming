4e2e87fe38e81eb5e4ec49ae95ae39036f5acd52
Lawisky24 <onyelawnwa@gmail.com>
2024-03-28 17:41:17 -0700

    task 2

---
+++ 0x18-dynamic_libraries/mod.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * mod - module of two integers
+ * @a: first unit
+ * @b: second unit
+ * Return: final result
+ */
+
+int mod(int a, int b)
+{
+	return (a % b);
+}
---
+++ 0x18-dynamic_libraries/sub.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * sub - subtract two integers
+ * @a: first unit
+ * @b: secind unit
+ * Return: subtrated final result
+ */
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
